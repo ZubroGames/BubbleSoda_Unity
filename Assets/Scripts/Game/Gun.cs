c3b10719d6b7dcb9ccf6dac37200ac1397feea16
HoodV <xhoodv@gmail.com>
2014-11-02 18:56:21 +0200

    init
    unity project with game scene

---
+++ Assets/Scripts/Game/Gun.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+	public static float rotate_speed = 80.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// rotating to pointer
+		float rotation_angle = 0.0f;
+		Vector3 rotation_axis = Vector3.right;
+		transform.rotation.ToAngleAxis(out rotation_angle, out rotation_axis);
+
+		float mouse_angle = Vector3.Angle (Input.mousePosition, Vector3.right);
+
+		float diff_angle = Mathf.Abs(mouse_angle - rotation_angle);
+		if (diff_angle > 3.0f)
+		{
+			Vector3 rotate_to;
+
+			if (Input.mousePosition.z < rotation_angle) {
+					rotate_to = Vector3.forward;
+			} else {
+					rotate_to = Vector3.back;
+			}
+
+			transform.Rotate (rotate_to, rotate_speed * Time.deltaTime);
+		}
+		else
+		{
+			// transform.rotation = Quaterion.AngleAxis
+		}
+	}
+}
