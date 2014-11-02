a66452443e089fe6096093a9fcfa33a09e03ebd9
HoodV <xhoodv@gmail.com>
2014-11-02 20:52:33 +0200

    gun rotation basic

--- Assets/Scripts/Game/Gun.cs
+++ Assets/Scripts/Game/Gun.cs
@@ -3,39 +3,17 @@ using System.Collections;
 
 public class Gun : MonoBehaviour {
 
-	public static float rotate_speed = 80.0f;
-
 	// Use this for initialization
 	void Start ()
 	{
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	void Update()
 	{
-		// rotating to pointer
-		float rotation_angle = 0.0f;
-		Vector3 rotation_axis = Vector3.right;
-		transform.rotation.ToAngleAxis(out rotation_angle, out rotation_axis);
+		Vector3 relative_position = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+		float angle = Vector3.Angle (Vector3.right, relative_position);
 
-		float mouse_angle = Vector3.Angle (Input.mousePosition, Vector3.right);
-
-		float diff_angle = Mathf.Abs(mouse_angle - rotation_angle);
-		if (diff_angle > 3.0f)
-		{
-			Vector3 rotate_to;
-
-			if (Input.mousePosition.z < rotation_angle) {
-					rotate_to = Vector3.forward;
-			} else {
-					rotate_to = Vector3.back;
-			}
-
-			transform.Rotate (rotate_to, rotate_speed * Time.deltaTime);
-		}
-		else
-		{
-			// transform.rotation = Quaterion.AngleAxis
-		}
+		transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
 	}
 }
